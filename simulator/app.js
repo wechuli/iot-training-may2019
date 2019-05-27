3d761f37cd2a77217d74c29475d4d3a16739afb1
Wechuli <wechulipaul@yahoo.com>
2019-05-27 20:10:07 +0300

    simulator

---
+++ simulator/app.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+const MAXIMUM_TEMP = 30;
+const MINIMUM_TEMP = 20;
+const MAXIMUM_HUMIDITY = 100;
+const MINIMUM_HUMIDITY = 10;
+
+function dummyDevice() {
+    const randomTemp = 
+    const randomHumi = 
+//   console.log("Strange stuff");
+  axios
+    .post("http://un-habitat-backend.azurewebsites.net", { temp: randomTemp, humidity: randomHumi })
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error));
+}
+
+setInterval(() => {
+  dummyDevice();
+}, 3000);
