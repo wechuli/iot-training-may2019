38ba6760e827ee94bae16f9021cc6d533efa88e3
wechuli <wechulipaul@yahoo.com>
2019-05-28 13:43:29 +0300

    getting rid of node_modules folder

--- simulator/app.js
+++ simulator/app.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const express = require('express');
+
 
 const MAXIMUM_TEMP = 30;
 const MINIMUM_TEMP = 20;
@@ -10,6 +12,7 @@ function dummyDevice() {
     const randomTemp = 25;
     const randomHumi = 24;
 //   console.log("Strange stuff");
+
   axios
     .post("http://un-habitat-backend.azurewebsites.net", { temp: randomTemp, humidity: randomHumi })
     .then(response => console.log(response.data))
@@ -17,9 +20,8 @@ function dummyDevice() {
 }
 
 
-
 setInterval(() => {
   dummyDevice();
-}, 3000);
+}, 10000);
 
 
